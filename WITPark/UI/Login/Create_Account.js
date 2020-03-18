91b5ec60965c62b41355ea5383b5e29c032bd02e
hauntsmane <32107564+hauntsmane@users.noreply.github.com>
2020-03-18 17:37:06 -0400

    Update Create_Account.js

--- WITPark/UI/Login/Create_Account.js
+++ WITPark/UI/Login/Create_Account.js
@@ -1,10 +1,78 @@
 import React, { Component } from 'react'
 import { View, StyleSheet } from 'react-native'
+import React, { Component } from 'react';
 
-export default class Login extends Component {
 
-}
+export default class Register extends Component {
 
-const styles = StyleSheet.create({
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: '',
+      error: '',
+    };
+
     
-  })
+    this.handlePassChange = this.handlePassChange.bind(this);
+    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.dismissError = this.dismissError.bind(this);
+  }
+
+  dismissError() {
+    this.setState({ error: '' });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+
+    if (!this.state.username) {
+      return this.setState({ error: 'Username Required' });
+    }
+
+    if (!this.state.password) {
+      return this.setState({ error: 'Password Required' });
+    }
+
+    return this.setState({ error: '' });
+  }
+
+  handleUserChange(evt) {
+    this.setState({
+      username: evt.target.value,
+    });
+  };
+
+  handlePassChange(evt) {
+    this.setState({
+      password: evt.target.value,
+    });
+  }
+
+  render() {
+
+
+    return (
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          {
+            this.state.error &&
+            <h3 data-test="error" onClick={this.dismissError}>
+              <button onClick={this.dismissError}>✖</button>
+              {this.state.error}
+            </h3>
+          }
+          <label>User Name</label>
+          <input type="text" data-test="username" value={this.state.username} onChange={this.handleUserChange} />
+
+          <label>Password</label>
+          <input type="password" data-test="password" value={this.state.password} onChange={this.handlePassChange} />
+
+          <input type="submit" value="Log In" data-test="submit" />
+        </form>
+      </div>
+    );
+  }
+}
+export default Register;
