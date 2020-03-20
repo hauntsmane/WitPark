e1328f81df08359cf9e37c6d36a7a32a80c60631
hauntsmane <32107564+hauntsmane@users.noreply.github.com>
2020-03-20 15:16:45 -0400

    added

---
+++ WITPark/UI/Login/Routes.js
@@ -0,0 +1,27 @@
+import {Router, Stack, Scene} from 'react-native-router-flux';
+ 
+import Login from './UI/Login';
+import Register from './UI/Register';
+ 
+export default class Routes extends Component {
+    render() {
+        return (
+            <Router barButtonIconStyle ={styles.barButtonIconStyle}
+                hideNavBar={false} 
+                navigationBarStyle={{backgroundColor: '#1565c0',}} 
+                titleStyle={{color: 'white',}}
+            >
+                <Stack key="root">
+                <Scene key="login" component={Login} title="Login"/>
+                <Scene key="signup" component={Signup} title="Sign up"/>
+                </Stack>
+            </Router>
+        )
+    }
+}
+ 
+const styles = {
+    barButtonIconStyle: {
+        tintColor: 'white'
+    }
+}
