f41e047ed793c76d28fbbe44b12f5ae4b30870f1
Josh Malnati <malnatij@wit.edu>
2020-03-23 22:47:39 -0400

    updated lot detail layout

--- WITPark/UI/Parking/Lot_Detail.js
+++ WITPark/UI/Parking/Lot_Detail.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Image, Button } from 'react-native'
+import { View, Text, StyleSheet, ImageBackground, Button } from 'react-native'
 import { Grid, Col, Row } from 'react-native-easy-grid'
 
 class Lot_Detail extends Component {
     
-//lot occupancy
-//lot image
-//pass info
+//need to add directions and parking restrictions
     isLotFull = () => {
         let current = this.props.occ.split('/')[0]
         let max = this.props.occ.split('/')[1]
@@ -33,15 +31,25 @@ class Lot_Detail extends Component {
         return(
             <Grid>
                 <Row>
-                    <Text>Name: {lot}</Text>
+                    <Row>
+                        <Col>
+                            <Text>Name: {lot}</Text>
+                        </Col>
+                        <Col>
+                        
+                        </Col>
+                    </Row>
+                    <Row>
+                        <ImageBackground 
+                        style={{backgroundColor:'black'
+                        , width: '100%'
+                        , height: '100%'}} >
+                            <Text style={{color:'white', fontSize:15}}>{occ}</Text>
+                        </ImageBackground>
+                    </Row>
                 </Row>
                 <Row>
-                    <Text>Occupancy: {occ}</Text>
-                </Row>
-                <Row>
-                    <Image style={{backgroundColor:'black'
-                                , width: '100%'
-                                , height: '100%'}} />
+                    <Text>Parking Pass Info</Text> 
                 </Row>
                 <Row>
                     {this.isLotFull()}
