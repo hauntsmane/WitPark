f18baa4a14d61d2bcd4dbdff9ebb59b23e2cf28d
hauntsmane <hauntsmane@wit.edu>
2020-04-10 17:35:22 -0400

    added logout

--- WITPark/WITPark/UI/Header.js
+++ WITPark/WITPark/UI/Header.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+//import { View, Text, StyleSheet } from 'react-native'
+import { Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native'
+//import { Menu, MenuProvider, MenuOptions, MenuOption, MenuTrigger} from 'react-native-popup-menu'
+import {Actions} from 'react-native-router-flux';
+import { Grid, Col, Row } from 'react-native-easy-grid'  
 
 class Header extends Component {
     
+    signOut(){
+        Actions.login()
+    }
 
+   
     render() {
         return (
+            
             <View className='Header' style={styles.container}>
-                <Text className="logo" style={styles.logo}>WITPark</Text>
+             <Grid>
+             <Col>
+               <Text className="logo" style={styles.logo}>WITPark</Text>
+              </Col>
+              <Col>
+              <TouchableOpacity onPress={ this.signOut}>
+        <Text
+          style={styles.logout}>
+          Signout
+        </Text>
+      </TouchableOpacity>
+              </Col>
+              </Grid>
             </View>
-        )
+        );
     }
 }
 
@@ -25,7 +46,15 @@ const styles = StyleSheet.create({
     },
     logo: {
         color: 'white',
+        marginTop: 20,
         fontSize: 20,
         left: 20
+    },
+    logout: {
+        color: 'white',
+        marginTop: 25,
+        fontSize: 15,
+        left: 115
     }
   })
+  
