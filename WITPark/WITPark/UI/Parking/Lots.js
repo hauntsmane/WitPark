491ac2892bf4376ef81e99885b7b8ed0e7ea7d40
hauntsmane <hauntsmane@wit.edu>
2020-04-12 16:13:22 -0400

    parking pass buying

--- WITPark/WITPark/UI/Parking/Lots.js
+++ WITPark/WITPark/UI/Parking/Lots.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+import { View, Text, StyleSheet, ImageBackground, TouchableOpacity, Linking} from 'react-native'
 import { Grid, Row, Col} from 'react-native-easy-grid'
 
 import Lot from './Lot'
@@ -9,7 +9,7 @@ export default class Lots extends Component {
     render() {
         return(
             <Grid>
-                <ImageBackground source={require('../Images/Wentworth_Shield.png')} style={{width: '100%', height: '100%'}}>
+                <ImageBackground source={require('../Images/Wentworth_Shield.png')} style={{width: '100%', height: '90%'}}>
                     <Row>
                         <Lot name='West Lot' occ='125/155' pass="F/S, C, V" lat={42.336606} lon={-71.096264}/> 
                     </Row>
@@ -22,7 +22,17 @@ export default class Lots extends Component {
                     <Row>
                         <Lot name='Parker St Lot' occ='450/450' pass="C, O, G" lat={42.333573} lon={-71.094481}/> 
                     </Row>
+                    
                 </ImageBackground>
+                <Row>
+                    <TouchableOpacity
+                    style={styles.button}
+                    onPress={ ()=>{ Linking.openURL('https://wit.edu/public-safety/parking')}} 
+                    >
+                    <Text style={styles.buttonText}>Parking Pass Purchasing/Info</Text>
+                   
+                </TouchableOpacity>
+                    </Row>
             </Grid>
         );
     }
@@ -31,5 +41,18 @@ export default class Lots extends Component {
 const styles = StyleSheet.create({
     lots: {
         
+    }
+    , button: {
+        width: 300,
+        backgroundColor: '#000000',
+        borderRadius: 2,
+        marginVertical: 15,
+        paddingVertical: 1
+    }
+    , buttonText: {
+        fontSize: 16,
+        fontWeight: '500',
+        color: '#ffffff',
+        textAlign: 'center'
     }
   })
