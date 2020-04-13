21669ccbb298daff999a28acb1ea819cb959cdf3
gerardg11 <gerardg1@wit.edu>
2020-04-13 17:14:32 -0400

    moved file location

---
+++ WITPark/WITPark/db/connect.js
@@ -0,0 +1,22 @@
+const { Pool, Client } = require('pg')
+
+const pool = new Pool()
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+
+const res = await pool.query('SELECT NOW()')
+await pool.end()
+
+const client = new Client()
+await client.connect()
+const res = await client.query('SELECT NOW()')
+await client.end()
+
+PGHOST='localhost'
+PGUSER=process.env.USER
+PGDATABASE=process.env.USER
+PGPASSWORD=witpark
+PGPORT=7897
+
