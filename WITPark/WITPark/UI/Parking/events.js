1dc18718193b00c3a4ed9371b3d5125ed8e1dfc1
hauntsmane <hauntsmane@wit.edu>
2020-06-10 19:20:34 -0400

    updated events

--- WITPark/WITPark/UI/Parking/events.js
+++ WITPark/WITPark/UI/Parking/events.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native'
 import { Grid, Row, Col } from 'react-native-easy-grid'
 import {Actions} from 'react-native-router-flux';
-import Eventform from './eventfrom'
+//import Eventform from './eventform'
 
 export default class Events extends Component {
    
@@ -10,14 +10,26 @@ export default class Events extends Component {
    
     render() {
         return(
-            <Row>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={this.events} 
-                >
-            <Text style={styles.buttonText}>Events</Text>
-            </TouchableOpacity>
-            </Row>
+            <ScrollView>
+            <Grid>  
+            <Col>
+              <Row>
+              <Text style={styles.txt}>Latest</Text>
+              </Row>
+              <Row>
+               <Text style={styles.txt}>Insert latest events</Text> 
+              </Row>
+            </Col>
+            <Col>
+              <Row>
+               <Text style={styles.txt}>Upcoming</Text> 
+              </Row>
+              <Row>
+              <Text style={styles.txt}>Insert all events</Text> 
+              </Row>
+            </Col>
+            </Grid>
+            </ScrollView>
         )
     }
 }
@@ -29,20 +41,20 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       backgroundColor: 'white',
     },
-    signupTextCont: {
-      flexGrow: 1,
-      justifyContent: 'center',
-      alignItems: 'flex-end',
-      paddingVertical: 16,
-      flexDirection: 'row',
-    },
-    signupText: {
-      color: '#000000', 
-      fontSize:16,
-    },
-    signupButton: {
-        color: '#E7B004',
-        fontSize:16,
-        fontWeight: '500',
-    }
+    button: {
+      width: 300,
+      backgroundColor: '#000000',
+      borderRadius: 25,
+      marginVertical: 10,
+      paddingVertical: 12
+  },
+  buttonText: {
+      fontSize: 16,
+      fontWeight: '500',
+      color: '#E7B004',
+      textAlign: 'center'
+  },
+  txt: {
+    fontSize:20
+}
 });
