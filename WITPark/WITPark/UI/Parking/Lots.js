7101b092e490451a6b111dfd24610f69ef5f3f5d
Josh Malnati <malnatij@wit.edu>
2020-07-23 22:43:14 -0400

    parking lot data

--- WITPark/WITPark/UI/Parking/Lots.js
+++ WITPark/WITPark/UI/Parking/Lots.js
@@ -12,21 +12,42 @@ e = getOccupancy('EastLot')
 a = getOccupancy('AnnexLot')
 p = getOccupancy('ParkerStLot')*/
 
+const days = {0:'Sun', 1:'Mon', 2:'Tues', 3:'Wed', 4:'Thurs', 5:'Fri', 6:'Sat'}
+
+const data = require('../../data/lots_present.json');
+let date = new Date();
+let time = date.getHours()*100 + Math.ceil(date.getMinutes()/5)*5;
+let day = days[date.getDay()]
+
+if (time < 1100)
+    time = 1100;
+if (time > 1259)
+    time = 1259;
+
 export default class Lots extends Component {
 
     events() {
         Actions.events()
     }
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            WestOcc: data.West_Lot[day][time].toString() + '/155'
+            , EastOcc: data.East_Lot[day][time].toString() + '/200'
+            , AnnexOcc: data.Annex_Lot[day][time].toString() + '/100'
+            , ParkerOcc: data.Parker_Lot[day][time].toString() + '/450'
+        };
+      }
 
     render() {
         return(
             <Grid style={{backgroundColor:'#000'}}>
                 <ImageBackground source={require('../Images/campus.jpg')} style={{width: '100%', height: '100%'}}>
-                    <Lot name='West Lot' occ='125/155' pass="F, V, M" lat={42.336606} lon={-71.096264}/> 
-                    <Lot name='East Lot' occ='188/200' pass="O" lat={42.336412} lon={-71.094252}/> 
-                    <Lot name='Annex Lot' occ='29/100' pass="C, O" lat={42.335235} lon={-71.093382}/> 
-                    <Lot name='Parker St Lot' occ='450/450' pass="C, O" lat={42.333573} lon={-71.094481}/> 
+                    <Lot name='West Lot' occ={this.state.WestOcc} pass="F, V, M" lat={42.336606} lon={-71.096264}/> 
+                    <Lot name='East Lot' occ={this.state.EastOcc} pass="O" lat={42.336412} lon={-71.094252}/> 
+                    <Lot name='Annex Lot' occ={this.state.AnnexOcc} pass="C, O" lat={42.335235} lon={-71.093382}/> 
+                    <Lot name='Parker St Lot' occ={this.state.ParkerOcc} pass="C, O" lat={42.333573} lon={-71.094481}/> 
 
                     <Row>
                         <TouchableOpacity
